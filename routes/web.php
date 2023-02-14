eff7621cd86930e664bcd3a4c741241012c904e0
cerry24 <samuelecerretti@gmail.com>
2023-02-14 17:45:34 +0100

    fix store route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/comics', [ComicsController::class, 'index'])->name('comics.index');
 Route::get('/comics/create', [ComicsController::class, 'create'])->name('comics.create');
+Route::post('/comics', [ComicsController::class, 'store'])->name('comics.store');
 Route::get('/comics/{comic}', [ComicsController::class, 'show'])->name('comics.show');
-Route::post('/', [ComicsController::class, 'store'])->name('comics.store');
